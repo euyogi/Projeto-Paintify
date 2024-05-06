d4435530f3f9c4a1f96216e6169128db4680afa8
yogi <yogiqhz@gmail.com>
2024-05-06 17:01:34 -0300

    config git

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import git
+
 app = Flask(__name__)
  
+@app.route('/update', methods=["POST"])
+def webhook():
+    if request.method == "POST":
+        repo = git.Repo("https://github.com/euyogi/Trabalho-OO")
+        origin = repo.remotes.origin
+        origin.pull()
+        
+        return "Updated PythonAnywhere successfully", 200
+    else:
+        return "Wrong event type", 400
+
+
 @app.route('/')
 def ola_mundo():
     nome = "glauco"
     return render_template("index.html", visitante=nome)    
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
