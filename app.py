ae83c1ac4dc8ba1ec367393c6b635bff74b42b78
euyogi <yogiqhz@gmail.com>
2024-05-06 21:42:43 -0300

    1

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 import git
 
 app = Flask(__name__)
- 
+
+
 @app.route("/update", methods=["POST"])
 def webhook():
     if request.method == "POST":
@@ -19,8 +20,9 @@ def webhook():
 def index():
     # Exemplo de variáveis que você pode passar para o template
     titulo = "Exemplo de Flask"
-    numeros = [1, 2, 3, 4, 5]
+    numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     return render_template('index.html', titulo=titulo, numeros=numeros)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
