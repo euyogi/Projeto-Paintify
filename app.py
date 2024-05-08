dadc17b8e001db145ecfb69ba46573e578c92c41
euyogi <yogiqhz@gmail.com>
2024-05-08 16:14:25 -0300

    added cadastros (/usuario) e necessarios

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import git
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minhabase.sqlite3'
+db = SQLAlchemy(app)
+
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, unique=True)
+    senha = db.Column(db.String)
+
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
 
 
 @app.route("/update", methods=["POST"])
@@ -24,5 +39,19 @@ def index():
     return render_template('index.html', titulo=titulo, numeros=numeros)
 
 
+@app.route("/usuario", methods=['POST', 'GET'])
+def addUsuario():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        user = Usuario(nome, senha)
+        db.session.add(user)
+        db.session.commit()
+
+    users = Usuario.query.all()
+    return render_template('usuario.html', usuarios=users)
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
