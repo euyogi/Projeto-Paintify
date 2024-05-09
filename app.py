63754401f79630e82c49ac202daa46f537cbf563
euyogi <yogiqhz@gmail.com>
2024-05-08 22:32:54 -0300

    upload com registro

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, url_for, redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 import git
 import os
 
+UPLOAD_FOLDER = "C://Users/yogiq/OneDrive/Documentos/PyCharm/Trabalho-OO/upload/"
+# UPLOAD_FOLDER = "/home/euyogi2/Trabalho-OO/upload/"
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///minhabase.sqlite3"
-UPLOAD_FOLDER = "/home/euyogi2/Trabalho-OO/upload/"
-# UPLOAD_FOLDER = "C://Users/yogiq/OneDrive/Documentos/PyCharm/Trabalho-OO/upload/"
+app.secret_key = "123456"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 db = SQLAlchemy(app)
 
 
@@ -23,6 +26,7 @@ class Usuario(db.Model):
         self.senha = senha
 
 
+# Whenever this page gets a post request it pulls my repo and reloads the website
 @app.route("/update", methods=["POST"])
 def webhook():
     if request.method == "POST":
@@ -35,34 +39,75 @@ def webhook():
         return "Wrong event type", 400
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    # Exemplo de variáveis que você pode passar para o template
-    titulo = "Exemplo de Flask"
-    numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    return render_template("index.html", titulo=titulo, numeros=numeros)
+    if "username" in session:
+        return "Logado como {}. <a href=\"/logout\">Deslogar</a>".format(session["username"])
+
+    return "Você não está logado. <a href=\"/login\">Logar</a>"
 
 
-@app.route("/usuario", methods=["POST", "GET"])
-def addUsuario():
+@app.route("/usuarios", methods=["POST", "GET"])
+def usuarios():
     if request.method == "POST":
-        nome = request.form["nome"]
-        senha = request.form["senha"]
+        nome = request.form["username"]
+        senha = request.form["password"]
         user = Usuario(nome, senha)
         db.session.add(user)
         db.session.commit()
 
     users = Usuario.query.all()
-    return render_template("usuario.html", usuarios=users)
+    return render_template("usuarios.html", usuarios=users)
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if "username" in session:
+        return redirect(url_for("index"))
+
+    if request.method == "POST":
+        nome = request.form["username"]
+        senha = request.form["password"]
+        if Usuario.query.filter_by(nome=nome).first() is None:
+            user = Usuario(nome, senha)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for("login"))
+
+    return render_template("signup.html", falhou=request.method == "POST")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if "username" in session:
+        return redirect(url_for("index"))
+
+    if request.method == "POST":
+        nome = request.form["username"]
+        senha = request.form["password"]
+        if Usuario.query.filter_by(nome=nome, senha=senha).first() is not None:
+            session["username"] = nome
+            return redirect(url_for("upload"))
+
+    return render_template("login.html", falhou=request.method == "POST")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 
 @app.route("/upload", methods=["POST", "GET"])
 def upload():
+    if "username" not in session:
+        return redirect(url_for("index"))
+
     if request.method == "POST":
-        file = request.files["arquivo"]
+        file = request.files["file"]
         save_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(save_path)
-        return "upload feito com sucesso"
+        return "upload feito com sucesso <a href=\"/logout\">Deslogar</a>"
 
     return render_template("upload.html")
 
